7a60ab24c1f2ef01ffd1f5e7510009cd13b6d022
Malte Hübner <maltehuebner@gmx.org>
2020-12-19 01:56:36 +0100

    Fix dependency injection and services.

--- src/LuftApiBundle.php
+++ src/LuftApiBundle.php
@@ -2,9 +2,14 @@
 
 namespace Caldera\LuftApiBundle;
 
+use Caldera\LuftApiBundle\DependencyInjection\CalderaLuftApiExtension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class LuftApiBundle extends Bundle
 {
-
+    public function getContainerExtension(): Extension
+    {
+        return new CalderaLuftApiExtension();
+    }
 }
